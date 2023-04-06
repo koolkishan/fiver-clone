8cfe1b6677fe2acb48b1ceb2a3386089c77fc116
koolkishan <kishansheth21@gmail.com>
2023-04-06 19:02:29 +0530

    Added Favicon and page title

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,10 +4,15 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Provider } from "react-redux";
 import { store } from "@/app/store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>Fiverr Clone</title>
+      </Head>
       <div className="relative">
         <Navbar />
         <Component {...pageProps} />
